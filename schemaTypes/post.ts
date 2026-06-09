7571bac61ecb1b0dd43d1ef4d9cc67518d1ba2f0
althaf <abdulla@innoventes.co>
2026-06-09 07:26:30 +0530

    update schema structure

---
+++ schemaTypes/post.ts
@@ -0,0 +1,19 @@
+// schemaTypes/post.ts
+
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+  ],
+};

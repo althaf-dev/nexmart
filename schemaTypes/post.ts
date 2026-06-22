5b3b1578f013fb5da791da39403d225c9f6ed24a
Althaf <abdulla@innoventes.co>
2026-06-22 10:35:12 +0530

    add products schema

--- schemaTypes/post.ts
+++ schemaTypes/post.ts
@@ -1,19 +1,31 @@
 // schemaTypes/post.ts
 
 export default {
-  name: "post",
-  title: "Post",
-  type: "document",
+  name: 'post',
+  title: 'Post',
+  type: 'document',
   fields: [
     {
-      name: "title",
-      title: "Title",
-      type: "string",
+      name: 'title',
+      title: 'Title',
+      type: 'string',
     },
     {
-      name: "description",
-      title: "Description",
-      type: "text",
+      name: "featuredImage",
+      title: "Featured Image",
+      type: "image",
+    },
+    {
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+    },
+
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{type: 'block'}],
     },
   ],
-};
+}
---
+++ schemaTypes/product.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'products',
+  title: 'products',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name:"featureImage",
+      title:"feature Image",
+      type: 'image'
+    }
+  ],
+}
